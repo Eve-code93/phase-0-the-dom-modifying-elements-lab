485af3a5e1a79ded7d8b7c3525832a0dcba3d676
Eve-code93 <evelynejose1993@gmail.com>
2025-03-02 20:33:35 +0300

    Updated index.js to pass Mocha/Chai DOM tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+//// Remove the <main> element with id "main"
+document.querySelector("main#main").remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set its id to "victory"
+newHeader.id = "victory";
+
+// Set its text content
+newHeader.innerHTML = "Your-Name is the champion"; // Replace 'Your-Name' with your actual name
+
+// Append the new <h1> to the body
+document.body.appendChild(newHeader);
